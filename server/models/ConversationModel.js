830fffcb18ef72227e7554229de1b508934dee1d
bqdiu <boquangdieu2003@gmail.com>
2024-07-14 01:33:53 +0700

    connect db, create models

---
+++ server/models/ConversationModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        default: ""
+    },
+    imageURL: {
+        type: String,
+        default: ""
+    },
+    videoURL: {
+        type: String,
+        default: ""
+    },
+    seen: {
+        type: Boolean,
+        default: false
+    }
+},{
+    timestamps: true
+});
+
+const ConversationSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        ref: 'User'
+    },
+    receiver: {
+        type: mongoose.Schema.ObjectId,
+        require: true,
+        ref: 'User'
+    },
+    message: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Message'
+        }
+    ]
+}, {
+    timestamps: true
+});
+
+const MessageModel = mongoose.model('Message', MessageSchema);
+const ConversationModel = mongoose.model('Conversation', ConversationSchema);
+
+module.exports = {
+    MessageModel,
+    ConversationModel
+};
---
+++ server/models/UserModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'provide name']
+    },
+    emai: {
+        type: String,
+        require: [true, 'provide email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [true, 'provide password'],
+    },
+    profile_pic: {
+        type: String,
+        default: ""
+    }
+}, {
+    timestamps: true
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
